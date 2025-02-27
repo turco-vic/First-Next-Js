36aa2a9c6824ee646920595af2721887c152cfc7
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-02-27 11:36:16 -0300

    Update Files

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             <main className={styles.main}>
                 <div className={styles.block}>
                     <article className={styles.article}>
-                        <Card content="Dois hambúrgueres suculentos, queijo derretido, alface crocante, cebola fresca, picles e aquele molho especial que só o McDonald's tem. Tudo isso entre três camadas de pão macio e levemente tostado. O Big Mac não é apenas um sanduíche, é uma experiência que desperta todos os sentidos!"/>
+                        <Card content=" O Big Mac não é apenas um sanduíche, é uma experiência que desperta todos os sentidos!"/>
                         <Card content="Cada mordida é uma explosão de sabores e texturas: o suco dos hambúrgueres, o cremoso do queijo, o frescor do alface e o toque único do molho especial. É o equilíbrio perfeito entre tradição e sabor inconfundível."/>
                         <Card content="O Big Mac é mais que um sanduíche: é um símbolo de momentos felizes, de encontros com amigos, de prazeres simples que fazem a vida valer a pena. E aí, vai perder a chance de provar essa delícia?"/>
                     </article>
